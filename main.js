8401ecf7b6cbbcecb80f0e117f09968aec4748d9
bek123456789 <redmirating@gmail.com>
2024-04-16 09:50:04 +0500

    done

---
+++ main.js
@@ -0,0 +1,119 @@
+// 1
+
+
+// let son = 5;
+// let bolish = 2
+// let qoldiq = son % bolish
+
+// if (qoldiq === 0) {
+//   alert("Qoldiq yo'q");
+// } else {
+//   alert("Qoldiq: " + qoldiq);
+// }
+
+
+
+
+// 2
+
+
+
+
+
+// let randomSon = Math.floor(Math.random() * 10) + 1;
+// alert(`Randomdan chiqgan son: ` + randomSon);
+
+
+
+
+// 3
+
+// let son = 12.510;
+// let butunSon = Math.floor(son);
+// alert("Butun son: " + butunSon);
+
+
+
+
+// 4
+
+
+// function harfniUzunligi(str) {
+//     return str.length;
+//   }
+  
+//   let word = "MARS IT SCHOOL";
+//   let wordLength = harfniUzunligi(word);
+//   alert("Sozda " + wordLength + " ta harf bor.");
+  
+
+
+
+
+// 5
+
+
+// function mars() {
+//     for (let i = 0; i < 10; i++) {
+//         console.log("MARS IT SCHOOL");
+      
+//     }
+// }
+
+// mars();
+
+
+// 6
+
+
+// let harflar = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+
+// let ism = "Bekzod"; 
+// let chiqarilishiKerak = false;
+
+// for (let i = 0; i < ism.length; i++) {
+//     if (harflar.indexOf(ism[i]) === -1) {
+//         chiqarilishiKerak = true;
+//         break;
+//     }
+// }
+
+// if (chiqarilishiKerak) {
+//     console.log(ism);
+// } 
+
+
+// 7
+
+
+
+
+// let age = prompt (`Yoshingizni kiriting`);
+// if (age >= 18){
+//     console.log(`Siz balog'at yoshiga yetgansiz`);
+// } else{
+//     console.log(`Siz balog'at yoshiga yetmagansiz`);
+// }
+
+
+
+// 8
+
+// let ism = prompt("Ismingizni kiriting:");
+// console.log(`Sizning ismingiz: ${ism.split("").reverse().join("")}`);
+
+
+
+// 10
+
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let son of sonlar) {
+//   if (son % 2 === 0) {
+//     console.log(son);
+//   }
+// }
+
+
+
